9288150978b9d8121952b360c247ea7102747f5a
Marlus Marcos <marlus@imd.ufrn.br>
2026-05-05 10:42:24 -0300

    feat: criação inicial do projeto backend

---
+++ Agendamente-Servicos-carro/src/main/java/br/ufrn/imd/agendamenteservicoscarro/AgendamenteServicosCarroApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.agendamenteservicoscarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamenteServicosCarroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamenteServicosCarroApplication.class, args);
+    }
+
+}
---
+++ Agendamente-Servicos-carro/src/test/java/br/ufrn/imd/agendamenteservicoscarro/AgendamenteServicosCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.agendamenteservicoscarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamenteServicosCarroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
